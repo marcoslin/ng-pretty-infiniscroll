8f2851e42e18daf9931d29e39078a517d0de0a50
Marcos Lin <linm.linmair@ebacimail.com>
2014-01-24 16:00:55 +0100

    Renamed standard.html to native.html; completed initial version of tiny scrollbar

--- www/js/app.js
+++ www/js/app.js
@@ -1,26 +1,71 @@
 /*global angular */
 
 var app = angular.module("webapp", ['ngRoute']);
-    
+
+/* ------------
+   ROUTE CONFIG
+*/
 app.config(['$routeProvider', function ($routeProvider) {
     'use strict';
     
     $routeProvider
-        .when('/home', {templateUrl: 'views/home.html', controller: 'HomeController'})
+        .when('/home', {templateUrl: 'views/home.html'})
+        .when('/tinyscrollbar', {templateUrl: 'views/tinyscrollbar.html', controller: 'DataController'})
         .otherwise({redirectTo: '/home'});
 
 }]);
 
 
-app.controller("HomeController", ['$scope', function ($scope) {
+
+
+/* ------------
+   DIRECTIVES
+*/
+app.directive("triggerScrollbar", ['$log', function ($log) {
+    'use strict';
+    return {
+        restrict: 'A',
+        link: function (scope, elm, attr) {
+            if (scope.$last) {
+                $log.log("Setting TinyScrollbar");
+                $('#tinyscrollbar1').tinyscrollbar();
+            }
+        }
+    };
+}]);
+
+
+
+
+/* ------------
+   FACTORY/SERVICE
+*/
+app.factory("DataSimple", ['$q', '$timeout', function ($q, $timeout) {
     'use strict';
     
-    var data = [], i;
+    var d = $q.defer();
     
-    for (i = 0; i <= 100; i += 1) {
-        data.push({ line: i, message: "Line number " + i});
-    }
-    
-    $scope.data = data;
+    $timeout(function () {
+        var result = [], i;
+        for (i = 0; i <= 100; i += 1) {
+            result.push("Entry number " + i);
+        }
+        d.resolve(result);
+    }, 0);
 
+    return d.promise;
+
+}]);
+
+
+
+
+/* ------------
+   CONTROLLERS
+*/
+app.controller("DataController", ['$scope', 'DataSimple', function ($scope, DataSimple) {
+    'use strict';
+    DataSimple.then(function (data) {
+        $scope.data = data;
+    });
 }]);
